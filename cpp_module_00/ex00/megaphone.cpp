3d18c1ed15ae8576a7fac4e76eebd98623ad054d
Mariam Eid <meid@lab1r5s10.42abudhabi.ae>
2025-07-06 19:11:03 +0400

    a

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -3,7 +3,10 @@
 
 int main(int ac, char **av) {
     if (ac == 1)
-    std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    {
+        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        return 0;
+    }
     int j = 1;
     while (av[j])
     {
