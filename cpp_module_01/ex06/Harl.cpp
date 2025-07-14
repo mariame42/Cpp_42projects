595663c85b7d1b332c3c2d9c3b90d2e13ef38c2f
Mariam Eid <meid@lab2r1s12.42abudhabi.ae>
2025-07-14 16:09:53 +0400

    a

--- cpp_module_01/ex06/Harl.cpp
+++ cpp_module_01/ex06/Harl.cpp
@@ -6,11 +6,27 @@
 /*   By: meid <meid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 18:51:59 by meid              #+#    #+#             */
-/*   Updated: 2025/07/06 18:57:46 by meid             ###   ########.fr       */
+/*   Updated: 2025/07/14 16:09:36 by meid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+void  Harl::debug(){
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(){
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(){
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error(){
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
 
 void Harl::switch_cace_fun (std::string level)
 {
@@ -27,13 +43,13 @@ void Harl::switch_cace_fun (std::string level)
 
     switch (lvl) {
         case 1:
-            std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+            debug();
         case 2:
-            std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+            info();
         case 3:
-            std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+            warning();
         case 4:
-            std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+            error();
             break;
         default:
             std::cout << "Invalid input";
--- cpp_module_01/ex06/Harl.hpp
+++ cpp_module_01/ex06/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: meid <meid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 18:40:49 by meid              #+#    #+#             */
-/*   Updated: 2025/07/14 16:02:12 by meid             ###   ########.fr       */
+/*   Updated: 2025/07/14 16:08:34 by meid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 #include "iostream"
 
 class Harl {
+    private:
+        void debug();
+        void info();
+        void warning();
+        void error();
     public:
         void switch_cace_fun (std::string level);
 };
